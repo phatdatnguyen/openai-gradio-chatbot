476c3a66d8b75153a411a6e038a82f8f4b04338c
Dat Nguyen <nguyenphatdat43@gmail.com>
2025-03-02 06:45:27 +0700

    add models

--- webui.py
+++ webui.py
@@ -218,7 +218,7 @@ def process_text(llm_model, temperature, top_p, text, history):
     return history
 
 def on_llm_model_change(llm_model):
-    if llm_model in ["gpt-3.5-turbo", "o1-preview", "o1-mini"]:
+    if llm_model in ["gpt-3.5-turbo", "o1", "o1-mini", "o1-preview", "o3-mini"]:
        image_input = gr.Image(label="Upload an image", sources=["upload", "clipboard"], type="pil", value=None, interactive=False)
     else:
        image_input = gr.Image(label="Upload an image", sources=["upload", "clipboard"], type="pil", value=None, interactive=True)
@@ -302,7 +302,7 @@ with gr.Blocks() as demo:
         with gr.Column(scale=1):
             text_input = gr.Textbox(label="Message", placeholder="Type a message or question...", autofocus=True)
             llm_model = gr.Dropdown(label="Model", value="gpt-4o-mini", choices=[
-                "gpt-3.5-turbo", "gpt-4", "gpt-4-turbo", "gpt-4o", "gpt-4o-mini", "chatgpt-4o-latest",  "o1-preview", "o1-mini"])
+                "gpt-3.5-turbo", "gpt-4", "gpt-4-turbo", "gpt-4o", "gpt-4o-mini", "chatgpt-4o-latest", "o1", "o1-mini", "o1-preview", "o3-mini"])
             temperature = gr.Slider(label="Temperature", minimum=0, maximum=2, step=0.01, value=1)
             top_p = gr.Slider(label="Top-p", minimum=0, maximum=1, step=0.01, value=1)
         with gr.Column(scale=1):
@@ -322,6 +322,6 @@ with gr.Blocks() as demo:
     save_button.click(save_history, [state, history_file_name], save_status)
     load_button.click(load_history, saved_history_file, [state, chatbot, load_status])
 
-demo.launch(max_file_size=5*gr.FileSize.MB)
+demo.launch(max_file_size=100*gr.FileSize.MB)
 
 
